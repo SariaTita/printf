6299f1dea2f15ac513542e9f199bf2d58b9706fe
SariaTita <sariaabbas12345@gmail.com>
2022-11-21 14:43:27 -0800

    -

--- main.h
+++ main.h
@@ -3,5 +3,15 @@
 #include <limits.h>
 #include <stdarg.h>
 #include <stdlib.h>
+/**
+* struct specifiers - struct specifiers
+* @specifier: the conversion specifier
+* @f: the function pointer
+**/
+Typedef struct specifiers
+{
+Char *specifier;
+Int(*f)(va_list args);
+} spc_dt;
 int _printf(const char *format, ...);
 #endif /* MAIN_H */
