6434a27a8cd6eb04c8ad825fdfd2c182f5abfdcc
SariaTita <sariaabbas12345@gmail.com>
2022-11-22 11:27:31 -0800

    -

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+#include <limits.h>
+#include <stdio.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
