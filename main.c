d1fd1596716a4fef71eabd358071f2a19f6ab05e
SariaTita <sariaabbas12345@gmail.com>
2022-11-22 12:34:07 -0800

    -

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - main function of program
+* Return: always 0
+*/
+int main(void)
+{
+int num;
+char *string;
+num = 98;
+string = "Hello!";
+_printf ("%s is %i.\n", string, num);
+return (0);
+}
